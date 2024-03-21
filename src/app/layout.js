9e1a2aeb93dd72238e90c8269e2df9dc7997042e
akhileshawachat <akhileshawachat65@gmail.com>
2024-03-22 04:09:38 +0530

    navlink,navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from ".app/components/navbar";
+import Navbar from "./components/navbar";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-red-500">
+        <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
+          <div className="h-24">
           <Navbar/>
-           {children}
+          </div>
+          <div classNme="h-[calc(100vh-16rem)]">{children}</div>
         </div>
       </body>
     </html>
